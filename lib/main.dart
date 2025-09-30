1faca32b6f897c39e97be3cd351ab9fd91884466
meminglr <mhmt.eminguler@gmail.com>
2025-09-30 10:27:08 +0300

    Add Firebase integration and configuration for Android and iOS
    - Updated build.gradle.kts and settings.gradle.kts to include Google services plugin.
    - Modified main.dart to initialize Firebase.
    - Added GoogleService-Info.plist and google-services.json for iOS and Android respectively.
    - Created firebase_options.dart for Firebase options management.
    - Updated pubspec.yaml and pubspec.lock to include Firebase dependencies.
    - Added Podfile for iOS dependencies management.
    - Created firebase.json for FlutterFire configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_learning/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -10,11 +14,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
